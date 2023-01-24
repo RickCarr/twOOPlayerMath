cddbe4bbb9783089555c4f9cbc0c09a8e6847d8d
RickCarr <rick.carr@telus.com>
2023-01-23 22:11:02 -0800

    Updating text output for styling

--- game.rb
+++ game.rb
@@ -5,15 +5,19 @@ class Game
   def initialize()    
     puts "      Welcome to Math Match!!"
     puts "Each player has 3 lives per Match"
+    puts ""
     print "Enter the first player's name: "
     @player1 = Player.new(gets.chomp)
-    puts "  #{@player1.name} Enters the Math Match..."        
+    puts "----#{@player1.name} Enters the Math Match----"
+    puts ""
     print "Enter the second player's name: "
     @player2 = Player.new(gets.chomp)
-    puts "  A New Challenger Appears!"    
-    puts "Next Math Match:"
-    puts "  #{@player1.name} Vs. #{@player2.name}!"    
-    puts "FIGHT!"    
+    puts "-----A New Challenger Appears-----"    
+    puts ""
+    puts "------------Math Match:-----------"
+    puts "        #{@player1.name} Vs. #{@player2.name}!"    
+    puts "--------------BEGIN!--------------"  
+    puts ""  
     @current_player = @player1
   end
   
@@ -31,11 +35,11 @@ class Game
 
       if guess != question.answer
         @current_player.lose_life
-        puts "Wrong! you lose a life. only #{@current_player.lives} remain"
+        puts "Wrong! you lose a life. #{@current_player.lives} remaining"
         puts ""
       else
-        puts "Check out the big brain on #{@current_player.name}!"
-        puts "    #{@current_player.lives} of 3 lives remaining"
+        puts "Correct! Check out the big brain on #{@current_player.name}!"
+        puts "     #{@current_player.lives} of 3 lives remaining"
         puts ""
       end
       if @current_player.lives == 0
@@ -43,14 +47,16 @@ class Game
       else   
         puts  "#{@player1.name}: #{@player1.lives}/3 Vs. #{@player2.name}: #{@player1.lives}/3"
         @current_player = @current_player == @player1 ? @player2 : @player1
+        puts""
         puts "--------------Next Turn--------------"
       end   
 
       def game_over
         winner = @player1.lives > @player2.lives ? @player1 : @player2
-        puts "!!!!!!!!!!!Congratulations!!!!!!!!!!!!"
+        puts "-----------Congratulations-----------"
+
         puts "#{winner.name} Wins! with #{winner.lives} of 3 lives remaining!"
-        puts "--------------Game Over.--------------"
+        puts "--------------Game Over--------------"
         exit(0)      
       end
     end
